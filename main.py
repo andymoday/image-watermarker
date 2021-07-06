3981eb1c1a301026c8faa1ceb8c0efd2b73f6a42
Andy Moday <andy.mopad@gmail.com>
2021-07-06 17:15:16 +0100

    add file dialog to save files

--- main.py
+++ main.py
@@ -10,33 +10,16 @@ BACKGROUND_COLOR = "#456456"
 
 # ------------------------------FUNCTIONS---------------------------------------- #
 def image_controller():
-    image = load_image()
+    images = load_image()
     watermark = load_watermark()
-    im_size = image.size
     wm_size = watermark.size
-    h_pos = int((im_size[0] / 2) - (wm_size[0] / 2))
-    v_pos = int((im_size[1] / 2) - (wm_size[1] / 2))
-    image.paste(watermark, (h_pos, v_pos), watermark.convert("RGBA"))
-    time.sleep(2)
-    save_image(image)
-
-
-def open_file_dialog(filetypes, type):
-    if type == "image":
-        message = "Choose an Image"
-    elif type == "watermark":
-        message = "Choose a Watermark"
-
-    filename = filedialog.askopenfilename(
-        title=message,
-        initialdir='/',
-        filetypes=filetypes)
-
-    showinfo(
-        title='Selected File',
-        message=filename
-    )
-    return filename
+    for image in images:
+        im_size = image.size
+        h_pos = int((im_size[0] / 2) - (wm_size[0] / 2))
+        v_pos = int((im_size[1] / 2) - (wm_size[1] / 2))
+        image.paste(watermark, (h_pos, v_pos), watermark.convert("RGBA"))
+        time.sleep(1)
+    save_images(images)
 
 
 def load_image():
@@ -45,10 +28,21 @@ def load_image():
         ('JPEG files', '*.jpg'),
         ('All files', '*.*')
     )
-    filename = open_file_dialog(filetypes, type="image")
-    im = Image.open(filename)
-    print(im.format)
-    return im
+    filenames = filedialog.askopenfilenames(
+        title="Choose Images",
+        initialdir='/',
+        filetypes=filetypes)
+
+    showinfo(
+        title='Selected Files',
+        message=filenames
+    )
+    ims = []
+    for im in filenames:
+        image = Image.open(im)
+        print(image.format)
+        ims.append(image)
+    return ims
 
 
 def load_watermark():
@@ -56,8 +50,16 @@ def load_watermark():
         ('PNG files', '*.png'),
         ('All files', '*.*')
     )
-    watermark = open_file_dialog(filetypes, type="watermark")
-    wm = Image.open(watermark)
+    filename = filedialog.askopenfilename(
+        title="Choose Watermark",
+        initialdir='/',
+        filetypes=filetypes)
+
+    showinfo(
+        title='Selected Files',
+        message=filename
+    )
+    wm = Image.open(filename)
     wm = wm.convert("RGBA")
     wm_data = wm.getdata()
 
@@ -72,17 +74,34 @@ def load_watermark():
     return wm
 
 
-def save_image(im):
-    im.save("outfile.png", "PNG")
+def save_images(ims):
+    filetypes = (
+        ('PNG files', '*.png'),
+        ('All files', '*.*')
+    )
+    for im in ims:
+        filename = filedialog.asksaveasfilename(
+            title="Save Image As:",
+            initialdir='/',
+            filetypes=filetypes,
+            defaultextension=".png",
+            initialfile=f"{im.filename}-watermarked")
+
+        showinfo(
+            title='Saved Files',
+            message=filename
+        )
+
+        im.save(filename, im.format)
 
 
 # ------------------------------SET UP UI---------------------------------------- #
 
 window = Tk()
 window.title("Image Watermarker")
-window.config(padx=110, pady=110, bg=BACKGROUND_COLOR)
+window.config(padx=105, pady=105, bg=BACKGROUND_COLOR)
 window.geometry("300x300")
-add_image_button = ttk.Button(text="Select Image", command=image_controller)
+add_image_button = ttk.Button(text="Select Images", command=image_controller)
 add_image_button.grid(row=0, column=0)
 add_image_button.pack()
 
